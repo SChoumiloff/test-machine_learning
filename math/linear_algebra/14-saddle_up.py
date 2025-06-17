84562c1ae54bbae0c6ab8158c3363ed1078ea967
SChoumiloff <schoumiloff@icloud.com>
2025-06-17 16:24:19 +0200

    fix: remove unnecessary blank line in np_matmul function

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -2,6 +2,7 @@
 """Write a function that performs matrix multiplication"""
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
     """Performs matrix multiplication between two given matrices"""
     return mat1 @ mat2
