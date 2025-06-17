f2d7737d0b4715730ad970ad2bb169db87c7415f
SChoumiloff <schoumiloff@icloud.com>
2025-06-17 16:16:20 +0200

    docs: enhance docstring for np_cat function to include detailed parameters and examples

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -7,4 +7,24 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
+    """
+    Concatenate two numpy arrays along a specified axis.
+
+    Parameters:
+    mat1 (numpy.ndarray): The first array to concatenate.
+    mat2 (numpy.ndarray): The second array to concatenate.
+    axis (int, optional): The axis along which the arrays will be concatenated. Default is 0.
+
+    Returns:
+    numpy.ndarray: The concatenated array.
+
+    Example:
+    >>> import numpy as np
+    >>> mat1 = np.array([[1, 2], [3, 4]])
+    >>> mat2 = np.array([[5, 6]])
+    >>> np_cat(mat1, mat2, axis=0)
+    array([[1, 2],
+           [3, 4],
+           [5, 6]])
+    """
     return np.concatenate((mat1, mat2), axis=axis)
