9695353dd0a9a828500265c73801d566d194e21b
SChoumiloff <schoumiloff@icloud.com>
2025-06-17 14:54:52 +0200

    feat: add add_arrays function to perform element-wise addition of two arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def add_arrays(arr1, arr2):
+    """
+    Add two arrays element-wise.
+
+    Args:
+        arr1 (list): First array.
+        arr2 (list): Second array.
+
+    Returns:
+        list: A new array with the element-wise sum of arr1 and arr2.
+
+    Raises:
+        SystemExit: If the arrays are not of the same length.
+    """
+    if len(arr1) != len(arr2):
+        exit(1)
+    return [arr1[i] + arr2[i] for i in range(len(arr2))]
