15ab8fd840f0e7c40c970331261befd2abf4f8c2
SChoumiloff <schoumiloff@icloud.com>
2025-06-17 16:23:10 +0200

    feat: implement matrix multiplication function and refine np_cat docstring

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,32 +1,8 @@
 #!/usr/bin/env python3
-"""
-concatenates two matrices along a specific axis
-"""
+"""Write a function that concatenates two matrices along a specific axis"""
+import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """
-    Concatenate two numpy arrays along a specified axis.
-
-    Parameters:
-    mat1 (numpy.ndarray): The first array to concatenate.
-    mat2 (numpy.ndarray): The second array to concatenate.
-    axis (int, optional): The axis along which the arrays
-    will be concatenated.
-    
-    Default is 0.
-
-    Returns:
-    numpy.ndarray: The concatenated array.
-
-    Example:
-    >>> import numpy as np
-    >>> mat1 = np.array([[1, 2], [3, 4]])
-    >>> mat2 = np.array([[5, 6]])
-    >>> np_cat(mat1, mat2, axis=0)
-    array([[1, 2],
-           [3, 4],
-           [5, 6]])
-    """
-    import numpy as np
-    return np.concatenate((mat1, mat2), axis=axis)
+    """Concatenates two given matrices along a specific axis"""
+    return np.concatenate((mat1, mat2), axis)
---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a function that performs matrix multiplication"""
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication between two given matrices"""
+    return mat1 @ mat2
