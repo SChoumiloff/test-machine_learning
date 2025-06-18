0365c791bad9faf3d17ab8a69246b11697fc2a6c
SChoumiloff <schoumiloff@icloud.com>
2025-06-18 11:09:24 +0200

    fix: handle edge cases in polynomial derivative function

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,8 +7,10 @@ Derive me plz
 def poly_derivative(poly):
     """Derive me plz
     """
-    if not poly or len(poly) == 0:
+    if not poly:
         return None
+    if len(poly) == 0:
+        return 0
     poly_new = []
     for i in range(1, len(poly)):
         poly_new.append(poly[i] * i)
