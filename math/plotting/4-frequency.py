f868027d390b9f33245ab0875677ebeb313703d4
SChoumiloff <schoumiloff@icloud.com>
2025-06-18 10:10:45 +0200

    feat: add frequency plotting function for student grades

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Lost frequencies
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Lost frequencies
+    """
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, 10, edgecolor='black')
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.show()
