1de9cbf111396a442b818770e1fa697a522a9adb
SChoumiloff <schoumiloff@icloud.com>
2025-06-19 10:59:53 +0200

    fix: correct typo in ValueError message for nx parameter validation in Neuron class

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,7 +12,7 @@ class Neuron:
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("nx must be a postive integer")
+            raise ValueError('nx must be a positive integer')
         self.nx = nx
         self.W = np.random.normal(size=(1, self.nx))
         self.b = 0
