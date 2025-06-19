d4c62ef74fdc74c4cdf931ef8825c7118da5911f
SChoumiloff <schoumiloff@icloud.com>
2025-06-19 14:51:12 +0200

    feat: implement Neuron class with forward propagation and cost calculation

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+No neuron 3
+"""
+import numpy as np
+
+
+class Neuron:
+    """No neuron 3
+    """
+    def __init__(self, nx: int):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, self.nx))
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """forward prop documentation
+        """
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Compute
+        """
+        m: int = Y.shape[1]
+        coef = np.float32(1/m)
+        assert Y.shape[1] == A.shape[1]
+        cost = coef * np.sum((A - Y)**2)
+        return cost
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
