a2574f670327ec55734a77483353f300fa06d728
SChoumiloff <schoumiloff@icloud.com>
2025-06-19 14:38:06 +0200

    feat: implement Neuron class with forward propagation and properties

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+No neuron 3
+"""
+import numpy as np
+
+
+class Neuron:
+    """No neuron 3
+    """
+    def __init__(self, nx: int):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+        self._W = np.random.normal(
+            loc=0,
+            scale=1,
+            size=(1, self.nx)
+        )
+        self._b = 0
+        self._A = 0
+
+    def forward_prop(self, X):
+        Z = np.matmul(self._W, X) + self._b
+        self._A = 1 / (1 + np.exp(-Z))
+
+    @property
+    def W(self):
+        return self._W
+
+    @property
+    def b(self):
+        return self._b
+
+    @property
+    def A(self):
+        return self._A
