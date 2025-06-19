fix: update cost calculation in Neuron class to use cross-entropy loss

supervised_learning/classification/3-neuron.py
@@ -29,9 +29,9 @@ class Neuron:
         """Compute
         """
         m: int = Y.shape[1]
-        coef = np.float32(1/m)
+        coef = - 1 / m
         assert Y.shape[1] == A.shape[1]
-        cost = coef * np.sum((A - Y)**2)
+        cost = coef * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.000000001 - A))
         return cost
 
     @property