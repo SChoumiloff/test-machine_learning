08c1425464888ee29fb6825ba93b2ae13cc42044
SChoumiloff <schoumiloff@icloud.com>
2025-06-19 10:41:10 +0200

    feat: add Neuron class implementation and update .gitignore

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+No neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """No Neuron
+    """
+    def __init__(self, nx: int):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise TypeError("nx must be a postive number")
+        self.nx = nx
+        self.W = np.random.normal(size=(1, self.nx))
+        self.b = 0
+        self.A = 0
